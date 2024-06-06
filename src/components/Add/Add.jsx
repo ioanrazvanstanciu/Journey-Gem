d3280192438b374f08995c1d11aa64a48b04dfdf
CodrutaVarga <vargacodruta22@gmail.com>
2024-06-06 18:25:01 +0300

    eroare date picker add page + stilizare

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
-import { AddContainer, AddButton } from "./Add.style";
-import AddForm from "./AddForm";
-
+import { AddContainer, AddButton , GlobalStyles } from "./Add.style";
+import {AddForm , AddFormDatePicker }from "./AddForm";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Add = () => {
+  const [ziCheckIn, setZiCheckIn] = useState(null);
+  const [ziCheckOut, setZiCheckOut] = useState(null);
   const [inputObject, setInputObject] = useState({
     tara: "",
     oras: "",
     imagine_pachet: "",
     nr_zile_concediu: 0,
-    zi_check_in: "",
-    zi_check_out: "",
+    zi_check_in: null,
+    zi_check_out: null,
     nr_pers: 0,
     mod_transport: "",
     pret_sejur: 0,
@@ -36,6 +37,7 @@ const Add = () => {
     setInputObject({ ...inputObject, [name]: e.target.value });
     handleError(e.target.value, name);
   };
+  
 
   const handleError = (value, name) => {
     switch (name) {
@@ -43,13 +45,91 @@ const Add = () => {
         if (value.length < 3) {
           setError({
             ...error,
-            [name]: "Titlu trebuie sa fie mai lung de 3 caracter",
+            [name]: "Numele tarii trebuie sa fie mai lung de 3 caractere",
           });
         } else if (value === "Title") {
           setError({ ...error, [name]: "titlu nepotrivit" });
-          // ...error copie oniectul vechi de erori
-          // [name] se refera la ["Title"]
-          // ce am facut e sa copi obiectul vechi si sa ii schimb numai title cu valoarea noua
+        } else {
+          setError({ ...error, [name]: undefined });
+        }
+        break;
+      case "oras":
+        if (value.length < 3) {
+          setError({
+            ...error,
+            [name]: "Numele orașului trebuie să fie mai lung de 3 caractere",
+          });
+        } else {
+          setError({ ...error, [name]: undefined });
+        }
+        break;
+      case "imagine_pachet":
+        if (!value) {
+          setError({
+            ...error,
+            [name]: "Trebuie să încărcați o imagine",
+          });
+        } else {
+          setError({ ...error, [name]: undefined });
+        }
+        break;
+      case "nr_zile_concediu":
+        if (value <= 0) {
+          setError({
+            ...error,
+            [name]: "Numărul de zile de concediu trebuie să fie mai mare decât 0",
+          });
+        } else {
+          setError({ ...error, [name]: undefined });
+        }
+        break;
+        case "zi_check_in":
+          case "zi_check_out":
+            if (!value) {
+              setError({
+                ...error,
+                [name]: `Trebuie să selectați o zi de ${name === "zi_check_in" ? "check-in" : "check-out"}`,
+              });
+            } else {
+              setError({ ...error, [name]: undefined });
+            }
+            break;
+      case "nr_pers":
+        if (value <= 0) {
+          setError({
+            ...error,
+            [name]: "Numărul de persoane trebuie să fie mai mare decât 0",
+          });
+        } else {
+          setError({ ...error, [name]: undefined });
+        }
+        break;
+      case "mod_transport":
+        if (!value) {
+          setError({
+            ...error,
+            [name]: "Trebuie să precizati un mod de transport",
+          });
+        } else {
+          setError({ ...error, [name]: undefined });
+        }
+        break;
+      case "pret_sejur":
+        if (value <= 0) {
+          setError({
+            ...error,
+            [name]: "Prețul sejurului trebuie să fie mai mare decât 0",
+          });
+        } else {
+          setError({ ...error, [name]: undefined });
+        }
+        break;
+      case "moneda_sejur":
+        if (!value) {
+          setError({
+            ...error,
+            [name]: "Trebuie să selectați o monedă pentru sejur",
+          });
         } else {
           setError({ ...error, [name]: undefined });
         }
@@ -58,9 +138,21 @@ const Add = () => {
         break;
     }
   };
+  const hasErrors = (errors) => {
+    const hasValidationErrors = Object.values(errors).some((error) => error !== undefined );
+    const allInputsEmpty = Object.values(inputObject).some((value) => value === "" || value === 0 || value=== null );
+    return hasValidationErrors || allInputsEmpty;
+  };
 
-  const handlesubmit = () => {
-    console.log(inputObject);
+  const handleSubmit = () => {
+    if (hasErrors(error)) {
+      if (Object.values(error).some((error) => error !== undefined)) {
+        toast.error("Nu poți trimite formularul până când toate erorile sunt rezolvate!", { autoClose: 5000 });
+      } else {
+        toast.error("Toate câmpurile trebuie completate!", { autoClose: 5000 });
+      }
+      return;
+    }
     fetch(`http://localhost:3001/pachete`, {
       method: "POST",
       body: JSON.stringify(inputObject),
@@ -69,36 +161,62 @@ const Add = () => {
       },
     })
       .then((response) => {
-        toast("Pachetul tau tocmai a fost modificat", {
-          autoClose: 5000,
-          onClose: () => {
-            window.location.reload();
-          },
-        });
+        if (response.ok) {
+          toast("Pachetul tau tocmai a fost adaugat!", {
+            autoClose: 5000,
+            onClose: () => {
+              window.location.reload();
+            },
+          });
+        } else {
+          throw new Error('Network response was not ok');
+        }
       })
       .catch((error) => {
         console.error("A aparut o eroare:", error);
-        toast.error("Eroare la modificarea pachetului!");
+        toast.error("Eroare la adaugarea pachetului!");
       });
   };
 
   return (
     <AddContainer>
-      {Object.keys(inputObject).map((el, index) => (
-        <AddForm
-          key={index}
-          name={el}
-          type={
-            el === "nr_zile_concediu" || el === "nr_pers" || el === "pret_sejur"
-              ? "number"
-              : "text"
-          }
-          value={inputObject[el]}
-          handleChange={handleChange}
-          error={error[el]}
-        />
-      ))}
-      <AddButton onClick={() => handlesubmit()}> Submit</AddButton>
+      <GlobalStyles /> 
+      {Object.keys(inputObject).map((el, index) => {
+        if (el === 'zi_check_in' || el === 'zi_check_out') {
+          return (
+  <AddFormDatePicker
+  key={index}
+  name={el}
+  selected={el === 'zi_check_in' ? ziCheckIn : ziCheckOut}
+  handleChange={(date) => {
+    if (el === 'zi_check_in') {
+      setZiCheckIn(date);
+    } else {
+      setZiCheckOut(date);
+    }
+    handleChange(date, el);
+  }}
+  error={error[el]}
+/>
+          );
+        } else {
+          return (
+            <AddForm
+              key={index}
+              name={el}
+              type={
+                el === "nr_zile_concediu" || el === "nr_pers" || el === "pret_sejur"
+                  ? "number"
+                  : "text"
+              }
+              value={inputObject[el]}
+              handleChange={handleChange}
+              error={error[el]}
+            />
+          );
+        }
+      })}
+      <AddButton onClick={handleSubmit}>Submit</AddButton>
       <ToastContainer />
     </AddContainer>
   );
--- src/components/Add/Add.style.js
+++ src/components/Add/Add.style.js
@@ -1,46 +1,122 @@
-import styled from "styled-components";
+import styled , {createGlobalStyle , css} from "styled-components";
+import { DARK_GREEN, LIGHT_GRAY, LIGHT_GREEN, PALE_YELLOW, WHITE_NEUTRAL } from "../../constants/color";
+import { ShieldExclamation } from "react-bootstrap-icons";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+export const StyledDatePicker = styled(DatePicker)`
+  height: 30px;
+  border-radius: 10px;
+  padding: 10px;
+  border: none;
+  border-bottom: 2px solid black;
+  background-color: ${PALE_YELLOW};
+  position: relative;
+  
+  &:hover {
+    border: 2px solid ${LIGHT_GREEN};
+  }
+
+  &:focus-within {
+    background-color: ${WHITE_NEUTRAL};
+    border: 2px solid ${DARK_GREEN};
+  }
+`;
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${WHITE_NEUTRAL};
+  }
+`;
 
 export const AddContainer = styled.div`
   display: flex;
-  justify-content: center;
-  text-align: center;
-  margin: 48px 0;
-  padding: 64px;
-  height: 100%;
-  gap: 16px;
-  flex-wrap: wrap;
   flex-direction: column;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  margin-top: 120px;
+  gap: 40px;
 `;
 
-export const AddLabel = styled.p`
-  font-size: large;
-  max-width: 400px;
-  margin: 12px auto 0;
+export const AddLabel = styled.label`
+  position: absolute;
+  left: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  transition: all 0.2s ease;
+  color: ${DARK_GREEN};
+  pointer-events: none;
+
+  ${({ isfocused, hasvalue }) =>
+    (isfocused || hasvalue) &&
+    css`
+      top: -12px;
+      font-size: 19px;
+      color: ${DARK_GREEN};
+    `}
 `;
 
 export const AddInput = styled.input`
-  max-width: 400px;
-  width: 100%;
-  height: 24px;
-  margin: 12px auto;
+  width: 50%;
+  height: 30px;
+  border-radius: 10px;
+  padding: 10px 10px 10px 35px;
+  border: none;
+  border-bottom: 2px solid black;
+  background-color: ${PALE_YELLOW};
+  position: relative;
+
+  &:hover {
+    border: 2px solid ${LIGHT_GREEN};
+  }
+
+  &:focus {
+    background-color: ${WHITE_NEUTRAL};
+    border: 2px solid ${DARK_GREEN};
+  }
+
+  &:focus + ${AddLabel}, &:not(:placeholder-shown) + ${AddLabel} {
+    top: -15px;
+    font-size: 19px;
+    color: ${DARK_GREEN};
+  }
 `;
 
 export const AddButton = styled.button`
-  max-width: 300px;
-  width: 100%;
-  height: 24px;
-  margin: 12px auto;
-  border-radius: 4px;
-  border: 1px solid #000;
+  margin: 25px 0;
+  width: 110px;
+  height: 40px;
+  border-radius: 10px;
   cursor: pointer;
+  border: 2px solid ${DARK_GREEN};
+  background-color: ${WHITE_NEUTRAL};
+  color: ${DARK_GREEN};
+  font-size: 17px;
+  font-weight: 700;
 
   &:hover {
-    background: gray;
-    color: white;
+    background-color: ${DARK_GREEN};
+    color: ${WHITE_NEUTRAL};
+  }
+
+  &:active {
+    background-color: ${PALE_YELLOW};
+    color: black;
+    border: 2px solid ${LIGHT_GREEN};
   }
 `;
 
 export const ErrorP = styled.p`
   color: red;
-  width: 100%;
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+`;
+
+export const ExclamationErrorIcon = styled(ShieldExclamation)`
+  width: 25px;
+  height: 25px;
+  margin: 0 10px;
 `;
